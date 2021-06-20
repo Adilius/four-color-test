11c47fd3161d045791c9a6b02bde44286536987e
Adilius <leugue-gamer@hotmail.com>
2021-06-20 18:02:22 +0200

    Paraphrased quiz questions

--- app/prediction_engine/engine.py
+++ app/prediction_engine/engine.py
@@ -1,18 +1,14 @@
-import enum
-
 answersheet = [
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"},
-    {"1": "green", "2": "blue", "3": "red", "4": "yellow"}
+    {"1": "red", "2": "green", "3": "yellow", "4": "blue"}, #1
+    {"1": "yellow", "2": "blue", "3": "red", "4": "green"}, #2
+    {"1": "yellow", "2": "green", "3": "red", "4": "blue"}, #3
+    {"1": "yellow", "2": "red", "3": "green", "4": "blue"}, #4
+    {"1": "green", "2": "blue", "3": "red", "4": "yellow"}, #5
+    {"1": "green", "2": "yellow", "3": "blue", "4": "red"}, #6
+    {"1": "red", "2": "green", "3": "blue", "4": "yellow"}, #7
+    {"1": "yellow", "2": "green", "3": "blue", "4": "red"}, #8
+    {"1": "yellow", "2": "green", "3": "blue", "4": "red"}, #9
+    {"1": "red", "2": "green", "3": "yellow", "4": "blue"} #10
 ]
 
 class prediction():
@@ -31,6 +27,4 @@ class prediction():
         highest_counters = [k for k,v in self.counter.items() if v == highest_counts]
 
         print(highest_counters)
-        pass
-        
         

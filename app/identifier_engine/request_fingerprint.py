cf4a4f784e046cf70535d9a9999b0b17c04d94da
Adilius <leugue-gamer@hotmail.com>
2021-06-21 17:03:06 +0200

    Added some fingerprinting

--- app/identifier_engine/request_fingerprint.py
+++ app/identifier_engine/request_fingerprint.py
@@ -1,5 +1,10 @@
 from flask import request
 
 def printAddr(request):
-    print("Printaddr:", request.remote_addr)
-    print("content_type:", request.content_type)
+    print("--------REQUEST HEADERS--------")
+    print("Accept_encodings:", request.accept_encodings)
+    print("Accept_languages:", request.accept_languages)
+    print("Accept_mimetypes:", request.accept_mimetypes)
+    print("Remote_addr:", request.remote_addr)
+    print("user_agent:", request.user_agent.string)
+    print("--------REQUEST HEADERS--------")

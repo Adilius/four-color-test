3ce3b34c389e46086e7dcff0f70ce6ca927afc73
Adilius <leugue-gamer@hotmail.com>
2021-07-02 21:10:24 +0200

    Clarified qualitative functions

--- app/prediction_engine/qualitative.py
+++ app/prediction_engine/qualitative.py
@@ -15,12 +15,14 @@ answersheet = [
     {1: "red", 2: "green", 3: "yellow", 4: "blue"} #10
 ]
 
+# Returns count of each personality type  
 def getCounts(choices):
     counter = { "green": 0, "blue": 0, "red": 0, "yellow": 0 }       # Counter for each type
     for count, choice in enumerate(choices):       # Count each color type
         counter[answersheet[count].get(choice)] += 1
     return counter
 
+# Returns base64 image plot of personality type plotted on graph
 def createPlot(choices):
     counter = getCounts(choices)
     img = io.BytesIO()
@@ -50,9 +52,10 @@ def createPlot(choices):
     ax.set_frame_on(False)
     plt.savefig(img, format='png', bbox_inches='tight', pad_inches = 0)
 
-    plot_url = base64.b64encode(img.getvalue()).decode()
-    return plot_url
+    plot_img = base64.b64encode(img.getvalue()).decode()
+    return plot_img
 
+# Returns list of predicted personality types
 def predict(choices):
     counter = getCounts(choices)
 
@@ -88,3 +91,46 @@ def predict(choices):
         colors.append('red')
 
     return colors, counter
+
+# Returns number instead of list of predicted personality type
+def predictNumber(choices):
+    
+    colors, counter = predict(choices)
+
+    if set(['green','yellow','blue','red']) == set(colors):
+        prediction = 1
+        print("1")
+
+    elif set(['yellow','red']) == set(colors):
+        prediction = 2
+        print('2')
+
+    elif set(['red']) == set(colors):
+        prediction = 3
+        print('3')
+
+    elif set(['blue','red']) == set(colors):
+        prediction = 4
+        print('4')
+
+    elif set(['blue']) == set(colors):
+        prediction = 5
+        print('5')
+
+    elif set(['green','blue']) == set(colors):
+        prediction = 6
+        print('6')
+
+    elif set(['green']) == set(colors):
+        prediction = 7
+        print('7')
+
+    elif set(['green','yellow']) == set(colors):
+        prediction = 8
+        print('8')
+
+    elif set(['yellow']) == set(colors):
+        prediction = 9
+        print('9')
+
+    return prediction, counter

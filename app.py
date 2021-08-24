cd3c39d157ff0646113c168912b88f92307e53a1
Adilius <leugue-gamer@hotmail.com>
2021-08-24 22:22:49 +0200

    Added certificate

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from app import app
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
             port=33507,
-            use_reloader = True)
+            use_reloader = True,
+            ssl_context=('cert.pem', 'privkey.pem'))
